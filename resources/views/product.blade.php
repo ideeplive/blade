e2f6ea71fe603af88e48b8b2980c00c26aae178a
Home <ideeplive@gmail.com>
2021-10-17 22:45:21 +0530

    space

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,19 +3,26 @@
 {{-- <h2>Use Of include</h2> --}}
 {{-- <h3>include @include('mobile')</h3> --}}
 {{-- <h3>include with array @include('mobile', ['name'=>'deep'])</h3> --}}
+
 {{-- <hr><hr> --}}
+
 {{-- <h2>Use Of includeIF {{$wn}}</h2> --}}
 {{-- <h3>includeIF @includeIf('mobile')</h3> --}}
 {{-- <h3>includeIF with array @includeIf('mobile', ['name' => 'deep'])</h3> --}}
+
 {{-- <hr><hr> --}}
+
 {{-- <h2>Use Of includeWhen</h2> --}}
 {{-- <h3>includeWhen @includeWhen($userType, 'mobile')</h3> --}}
 {{-- <h3>includeWhen with array @includeWhen($userType, 'mobile', ['total_mobile'=>4])</h3> --}}
+
 {{-- <hr><hr> --}}
+
 {{-- <h2>Use Of includeUnless</h2> --}}
 {{-- <h3>includeUnless @includeUnless($userType, 'mobile')</h3> --}}
 {{-- For Not Use include --}}
 {{-- <h3>includeUnless with array @includeUnless(!$userType, 'mobile', ['total_mobile'=>4])</h3> --}}
+
 {{-- <hr><hr> --}}
 
 
